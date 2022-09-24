1bb45831ee87126faafb1248f45eea29d86e00c0
김태영 <freeptk1@gmail.com>
2022-09-24 11:43:18 +0900

    Advice 순서를 바꿔서 해결하기

--- src/main/java/com/example/exceptionhandlertest/app/AControllerAdvice.java
+++ src/main/java/com/example/exceptionhandlertest/app/AControllerAdvice.java
@@ -11,9 +11,4 @@ public class AControllerAdvice {
     public ExceptionResponse runtimeException(RuntimeException e){
         return new ExceptionResponse(this.getClass().getSimpleName() + "의 runtimeException() 에서 처리!", e.getMessage());
     }
-
-    @ExceptionHandler(TestException.class)
-    public ExceptionResponse testException(TestException e){
-        return new ExceptionResponse(this.getClass().getSimpleName() + "의 testException() 에서 처리!", e.getMessage());
-    }
 }
--- src/main/java/com/example/exceptionhandlertest/app/b/BControllerAdvice.java
+++ src/main/java/com/example/exceptionhandlertest/app/b/BControllerAdvice.java
@@ -3,13 +3,13 @@ package com.example.exceptionhandlertest.app.b;
 
 import com.example.exceptionhandlertest.app.ExceptionResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+
+@DomainSpecificAdvice
 public class BControllerAdvice {
 
     @ExceptionHandler(TestException.class)
     public ExceptionResponse testException(TestException e){
-        return new ExceptionResponse(this.getClass().getSimpleName() + "에서 처리!", e.getMessage());
+        return new ExceptionResponse(this.getClass().getSimpleName() + "의 testException() 에서 처리!", e.getMessage());
     }
 }
