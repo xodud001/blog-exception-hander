48040c07ac87b33a7fac6bc18e490e5c7df6c538
김태영 <freeptk1@gmail.com>
2022-09-24 11:14:24 +0900

    ExceptionHandler를 합쳐서 해결

--- src/main/java/com/example/exceptionhandlertest/app/AControllerAdvice.java
+++ src/main/java/com/example/exceptionhandlertest/app/AControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.exceptionhandlertest.app;
 
+import com.example.exceptionhandlertest.app.b.TestException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,6 +9,11 @@ public class AControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ExceptionResponse runtimeException(RuntimeException e){
-        return new ExceptionResponse(this.getClass().getSimpleName() + "에서 처리!", e.getMessage());
+        return new ExceptionResponse(this.getClass().getSimpleName() + "의 runtimeException() 에서 처리!", e.getMessage());
+    }
+
+    @ExceptionHandler(TestException.class)
+    public ExceptionResponse testException(TestException e){
+        return new ExceptionResponse(this.getClass().getSimpleName() + "의 testException() 에서 처리!", e.getMessage());
     }
 }
